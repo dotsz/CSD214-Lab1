0420a5e83aee3a068f32b07af3c119d85dd64b19
dotsz <23023987@saultcollege.ca>
2024-09-26 11:15:23 -0400

    create Person class, implement constructors, getters/setters, and override toString, equals, and hascode methods

--- src/main/java/lab1/q3/Main.java
+++ src/main/java/lab1/q3/Main.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 public class Main implements Serializable {
     public static void main(String[] args) {
-        App app = new App();
-        app.run();
-
+        new App().run();
     }
 }
--- src/main/java/lab1/q3/Person.java
+++ src/main/java/lab1/q3/Person.java
@@ -1,59 +1,50 @@
 package lab1.q3;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Person implements Serializable {
     private String firstname;
     private String lastname;
     private Gender gender;
     private Integer SIN;
-    private static int COUNT;
+    private static int COUNT; // Object counter
+    public enum Gender{ Male, Female, Others } // Enums for the gender property of the Person class
 
     // Getters and Setters
     public String getFirstname() {
         return firstname;
     }
-
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-
     public Gender getGender() {
         return gender;
     }
-
     public void setGender(Gender gender) {
         this.gender = gender;
     }
-
     public Integer getSIN() {
         return SIN;
     }
-
     public void setSIN(Integer SIN) {
         this.SIN = SIN;
     }
-
     public static int getCOUNT() {
         return COUNT;
     }
-
     public static void setCOUNT(int COUNT) {
         Person.COUNT = COUNT;
     }
 
-    public enum Gender{ Male, Female, Others }
-
     // Constructors
-    public Person() {
+    public Person() { // Default empty parameter constructor
         this.firstname = "";
         this.lastname = "";
         this.SIN = 0;
@@ -82,7 +73,8 @@ public class Person implements Serializable {
 
     }
 
-    @Override // Override toString method
+    // Overrides for the toString, equals and hashCode methods
+    @Override
     public String toString(){
         if(this.getGender() == null) {
             return "Person: " + this.getFirstname() + " " + this.getLastname() + ", " + this.getSIN();
@@ -92,4 +84,21 @@ public class Person implements Serializable {
         }
 
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == null) return false; // check if the object is null
+        if(this == o) return true; // compare the memory address of the two objects
+        if(!(o instanceof Person person)) return false; // check if the object is an instance of the Person class
+        // compare property values of the two objects
+        return this.getFirstname().equals(person.getFirstname()) && this.getLastname().equals(person.getLastname()) && this.getSIN().equals(person.getSIN());
+    }
+
+    @Override
+    public int hashCode() {
+        // generate a hash code for the properties of the object
+        return Objects.hash(this.getFirstname(), this.getLastname(), this.getSIN());
+    }
+
+
 }
