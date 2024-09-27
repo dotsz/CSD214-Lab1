ac21123bb2fb19361bd703d4df5f37c23889e6fc
dotsz <vfelipejr@gmail.com>
2024-09-26 23:43:11 -0400

    add comments to BookTest unit

--- src/test/java/lab1/q4/BookTest.java
+++ src/test/java/lab1/q4/BookTest.java
@@ -8,14 +8,15 @@ class BookTest {
     @Test
     void testIfBookObjectsAreEquals() {
 
+        // Create a mock person object
         Person p1 = new Person("Joshua", "Bloch", Person.Gender.Male, 1234);
-
-
+        // Create 4 book objects
         Book b1 = new Book("Effective Java", p1, "978-0134685991");
         Book b2 = new Book("Effective Java", p1, "978-0134685991");
         Book b3 = new Book("Effective Java", p1, "978-0134685991");
         Book b4 = b1;
 
+        // Test if the book objects are equal
         assertEquals(b1, b2);
         assertEquals(b4, b1);
         assertEquals(b1, b3);
